4d0e4eb4098a1cbd7abeecaad3a368e99a44c3c8
Woongbbi <jeon.wbbi@gmail.com>
2019-03-20 00:39:55 +0900

    setup 스크립트 오류 수정

--- scripts/https-setup.sh
+++ scripts/https-setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ -z "$EMAIL" ] || [ -z "$DOMAIN" ] || [! -f /etc/nginx/site-available/default-ssl.conf ]; then
+if [ -z "$EMAIL" ] || [ -z "$DOMAIN" ] || [ ! -f "/etc/nginx/sites-available/default-ssl.conf" ]; then
  echo "You need the \$EMAIL and the \$DOMAIN variables and default-ssl.conf file"
 else
- if [! -d  "/etc/letsencrypt/live/$DOMAIN" ]; then
+ if [ ! -d  "/etc/letsencrypt/live/$DOMAIN" ]; then
     certbot certonly --webroot -w /var/www/html -d $DOMAIN --email $EMAIL --agree-tos --quiet
  fi
 
- if [! -f "/etc/letsencrypt/live/privkey.pem" ]; then
+ if [ ! -f "/etc/letsencrypt/live/privkey.pem" ]; then
     ln -s /etc/letsencrypt/live/$DOMAIN/privkey.pem /etc/letsencrypt/live/
  fi
 
- if [! -f "/etc/letsencrypt/live/fullchain.pem" ]; then
+ if [ ! -f "/etc/letsencrypt/live/fullchain.pem" ]; then
     ln -s /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/
  fi
 
- if [! -f "/etc/nginx/sites-enabled/default-ssl.conf" ]; then
+ if [ ! -f "/etc/nginx/sites-enabled/default-ssl.conf" ]; then
     ln -s /etc/nginx/sites-available/default-ssl.conf /etc/nginx/sites-enabled/
  fi
 
