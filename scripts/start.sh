8e5f386907f6fd08e88747ee29127649803d41d3
Woongbbi <jeon.wbbi@gmail.com>
2019-07-08 21:24:05 +0900

    mod script

--- scripts/start.sh
+++ scripts/start.sh
@@ -134,6 +134,32 @@ fi
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
 
 if [[ "$ENABLE_HTTPS" == "1" ]] ; then
- sh /https-setup.sh
+    echo "Enable-Https"
+
+    if [ -z "$WEBROOT" ] || [ -z "$EMAIL" ] || [ -z "$DOMAIN" ] || [ ! -f "/etc/nginx/sites-available/default-ssl.conf" ]; then
+     echo "You need the \$EMAIL and the \$DOMAIN variables and default-ssl.conf file"
+    else
+     if [ ! -d "/etc/letsencrypt/live" ]; then
+        mkdir /etc/letsencrypt/live
+     fi
+
+     if [ ! -d  "/etc/letsencrypt/live/$DOMAIN" ]; then
+        certbot certonly --webroot -w $WEBROOT -d $DOMAIN --email $EMAIL --agree-tos --quiet
+     fi
+
+     if [ ! -f "/etc/letsencrypt/live/privkey.pem" ]; then
+        ln -s /etc/letsencrypt/live/$DOMAIN/privkey.pem /etc/letsencrypt/live/
+     fi
+
+     if [ ! -f "/etc/letsencrypt/live/fullchain.pem" ]; then
+        ln -s /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/
+     fi
+
+     if [ ! -f "/etc/nginx/sites-enabled/default-ssl.conf" ]; then
+        ln -s /etc/nginx/sites-available/default-ssl.conf /etc/nginx/sites-enabled/
+     fi
+
+     nginx -s reload
+    fi
 fi
 
